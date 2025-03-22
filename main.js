5bf60c6510bd4afebd7e392700ac7dc55685f41d
mercy chepkemoi koech <koechmercyk@gmail.com>
2025-03-22 23:10:03 +0300

    simple liker

--- main.js
+++ main.js
@@ -2,8 +2,36 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+//your code here!!
+document.addEventListener("DOMContentLoaded", () => {
+ 
+  const errorModal = document.getElementById("modal");
+  errorModal.classList.add("hidden");
 
+  const hearts = document.querySelectorAll(".like-glyph");
+
+  hearts.forEach((heart) => {
+    heart.addEventListener("click", () => {
+      if (heart.innerText === EMPTY_HEART) {
+        mimicServerCall()
+          .then(() => {
+            heart.innerText = FULL_HEART;
+            heart.classList.add("activated-heart");
+          })
+          .catch((error) => {
+            errorModal.classList.remove("hidden");
+            errorModal.querySelector("#modal-message").innerText = error;
+            setTimeout(() => {
+              errorModal.classList.add("hidden");
+            }, 3000);
+          });
+      } else {
+        heart.innerText = EMPTY_HEART;
+        heart.classList.remove("activated-heart");
+      }
+    });
+  });
+});
 
 
 
